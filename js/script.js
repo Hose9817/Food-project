fbe1fa67358c941c6e94643cbafed69e18357318
Hose9817 <abdullayev6859@gmail.com>
2022-06-16 10:32:12 +0600

    added functional with default value of className

--- js/script.js
+++ js/script.js
@@ -170,17 +170,82 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalByScroll);
 
+    //Class template
+
+    // class MenuCard {
+    //     constructor(src, alt, title, descr, price, parentSelector) {
+    //         this.src = src;
+    //         this.alt = alt;
+    //         this.title = title;
+    //         this.descr = descr;
+    //         this.price = price;
+    //         this.parent = document.querySelector(parentSelector);
+    //         this.transfer = 29; // usd/uah exchange rate
+    //         this.changeToUAH();
+    //     }
+
+    //     changeToUAH() {
+    //         this.price = this.price * this.transfer;
+    //     }
+
+    //     render() {
+    //         const element = document.createElement('div');
+    //         element.innerHTML = ` 
+    //             <div class="menu__item">
+    //                 <img src=${this.src} alt=${this.alt}>
+    //                 <h3 class="menu__item-subtitle">${this.title}</h3>
+    //                 <div class="menu__item-descr">${this.descr}</div>
+    //                 <div class="menu__item-divider"></div>
+    //                 <div class="menu__item-price">
+    //                     <div class="menu__item-cost">Цена:</div>
+    //                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+    //                 </div>
+    //             </div>
+    //         `;
+    //         this.parent.append(element);
+    //     }
+    // }
+
+    //     new MenuCard(
+    //         "img/tabs/vegy.jpg",
+    //         "vegy",
+    //         'Меню "Фитнес"',
+    //         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.',
+    //         9,
+    //         '.menu .container'
+    //     ).render();
+
+    //     new MenuCard(
+    //         "img/tabs/elite.jpg",
+    //         "elite",
+    //         'Меню “Премиум”',
+    //         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.',
+    //         17,
+    //         '.menu .container'
+    //     ).render();
+
+    //     new MenuCard(
+    //         "img/tabs/post.jpg",
+    //         "post",
+    //         'Меню “Постное”',
+    //         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки.',
+    //         12,
+    //         '.menu .container'
+    //     ).render();
+
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
-            this.transfer = 25;
+            this.transfer = 29;
             this.changeToUAH();
         }
+
 
         changeToUAH() {
             this.price = this.price * this.transfer;
@@ -188,21 +253,26 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
-            element.innerHTML = ` 
-        <div class="menu__item">
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-        </div>
-    `;
+            if(this.classes.length){
+                this.classes.forEach(className => element.classList.add(className));
+            } else {
+                this.element = 'menu__item';
+                this.classes = element.classList.add(this.element);
+            }
+            
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
             this.parent.append(element);
-
         }
+
     }
 
     new MenuCard(
@@ -210,9 +280,30 @@ window.addEventListener('DOMContentLoaded', () => {
         "vegy",
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.',
-        9,
-        '.menu .container'
-
+        7,
+        '.menu .container',
     ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.',
+        17,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню “Постное”',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения.',
+        12,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+
 
 });
