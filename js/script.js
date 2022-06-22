d207142b740168b5296171d54806c6f2b95c814a
Hose9817 <abdullayev6859@gmail.com>
2022-06-22 11:25:10 +0600

    post data on localserver

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2022-06-30';
+    const deadline = '2022-06-23';
 
     function getTimeRemain(endtime) {
 
@@ -167,8 +167,8 @@ window.addEventListener('DOMContentLoaded', () => {
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
+    // window.addEventListener('scroll', showModalByScroll);
 
-    window.addEventListener('scroll', showModalByScroll);
 
     //Class template
 
@@ -304,6 +304,59 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
 
+
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Thank you! We well call you',
+        failure: 'Something get wrong...',
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+            // console.log(formData);
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if(request.status === 200){
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 
 
 });
